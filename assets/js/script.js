01d893632f138e2229c9dcd2092bfd9576789595
Jean Carlos <jcpds14@gmail.com>
2023-01-11 14:32:00 -0300

    finish fake blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,38 @@ async function readPosts() {
     }
 }
 
+async function addNewPost(title, body) {
+    await fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                uerId: 3
+            })
+
+
+        }
+    );
+
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+
+    readPosts();
+}
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+    const title = document.querySelector('#titleField').value;
+    const body = document.querySelector('#bodyField').value;
+
+    if (title && body) {
+        addNewPost(title, body);
+    } else {
+        alert('Os campos precisam ser preenchidos');
+    }
+});
+
 readPosts();
